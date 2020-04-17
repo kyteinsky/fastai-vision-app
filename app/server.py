2226501611538327b766dbd06e2be94f139e08e2
kyteinsky <kyteinsky@gmail.com>
2020-04-17 11:41:09 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -26,9 +26,9 @@ async def download_file(url, dest):
             with open(dest, 'wb') as f: f.write(data)
 
 async def setup_learner():
-    await download_file(model_file_url, path/'models'/f'{model_file_name}')
+    await download_file(model_file_url, path/f'{model_file_name}')
     try:
-        learn = load_learner(path, 'models'/model_file_name)
+        learn = load_learner(path, model_file_name)
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
